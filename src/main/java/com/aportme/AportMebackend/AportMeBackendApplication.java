8095956bb99831820e94d1bb731579313ecd949d
Wojciech Jarmosz <wojciech@jarmosz.com>
2020-03-23 21:52:55 +0100

    Initial commit

---
+++ src/main/java/com/aportme/AportMebackend/AportMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aportme.AportMebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AportMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AportMeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aportme/AportMebackend/AportMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aportme.AportMebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AportMeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
