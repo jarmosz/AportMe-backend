717e23c8664a4d1fccb22b2cba8ca0d64001cf49
blvckhype <Muzyka90!>
2020-03-24 18:22:33 +0100

    adds project structure and h2 db configuration

--- src/main/java/com/aportme/aportme/backend/AportMeBackendApplication.java
+++ src/main/java/com/aportme/aportme/backend/AportMeBackendApplication.java
@@ -1,4 +1,4 @@
-package com.aportme.AportMebackend;
+package com.aportme.aportme.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
