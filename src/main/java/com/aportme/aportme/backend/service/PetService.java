b13d19981c3310953bcbf8b6c696e35ee3f13594
poczekajcie <krakowski.jacek97@wp.pl>
2020-03-29 11:39:14 +0200

    add more CRUD methods

--- src/main/java/com/aportme/aportme/backend/service/PetService.java
+++ src/main/java/com/aportme/aportme/backend/service/PetService.java
@@ -1,7 +1,9 @@
 package com.aportme.aportme.backend.service;
 
 import com.aportme.aportme.backend.entity.pet.Pet;
+import com.aportme.aportme.backend.repository.FoundationInfoRepository;
 import com.aportme.aportme.backend.repository.PetRepository;
+import com.aportme.aportme.backend.utils.UtilsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import java.util.List;
 public class PetService {
 
     private final PetRepository petRepository;
+    private final FoundationInfoRepository foundationInfoRepository;
 
     public List<Pet> getAll() {
         return petRepository.findAll();
@@ -19,5 +22,30 @@ public class PetService {
 
     public Pet getById(Long id) {
         return petRepository.findById(id).orElse(null);
+    }
+
+    public Pet update(Long id, Pet pet) {
+        Pet dbPet = petRepository.findById(id).get();
+        UtilsService.copyNonNullProperties(pet, dbPet);
+        return petRepository.save(dbPet);
+    }
+
+    public Pet create(Long foundationId, Pet pet) {
+        Pet dbPet = new Pet();
+        dbPet.setAge(pet.getAge());
+        dbPet.setAgeCategory(pet.getAgeCategory());
+        dbPet.setAgeSuffix(pet.getAgeSuffix());
+        dbPet.setBehavioristNeeded(pet.getBehavioristNeeded());
+        dbPet.setBehaviorToAnimals(pet.getBehaviorToAnimals());
+        dbPet.setBehaviorToChildren(pet.getBehaviorToChildren());
+        dbPet.setBreed(pet.getBreed());
+        dbPet.setDescription(pet.getDescription());
+        dbPet.setDiseases(pet.getDiseases());
+        dbPet.setFoundationInfo(foundationInfoRepository.findById(foundationId).get());
+        dbPet.setName(pet.getName());
+        dbPet.setSize(pet.getSize());
+        dbPet.setPetType(pet.getPetType());
+        dbPet.setTrainingNeeded(pet.getTrainingNeeded());
+        return petRepository.save(dbPet);
     }
 }
--- src/main/java/com/aportme/aportme/backend/service/UserInfoService.java
+++ src/main/java/com/aportme/aportme/backend/service/UserInfoService.java
@@ -1,9 +1,37 @@
 package com.aportme.aportme.backend.service;
 
+import com.aportme.aportme.backend.entity.user.UserInfo;
+import com.aportme.aportme.backend.repository.UserInfoRepository;
+import com.aportme.aportme.backend.repository.UserRepository;
+import com.aportme.aportme.backend.utils.UtilsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserInfoService {
+
+    private final UserInfoRepository userInfoRepository;
+    private final UserRepository userRepository;
+
+    public UserInfo getById(Long id) {
+        return  userInfoRepository.findById(id).orElse(null);
+    }
+
+    public UserInfo update(Long id, UserInfo userInfo) {
+        UserInfo dbUserInfo = userInfoRepository.findById(id).get();
+        UtilsService.copyNonNullProperties(userInfo, dbUserInfo);
+        return userInfoRepository.save(dbUserInfo);
+    }
+
+    public UserInfo create(Long userId, UserInfo userInfo) {
+        UserInfo dbUserInfo = new UserInfo();
+        dbUserInfo.setPhoneNumber(userInfo.getPhoneNumber());
+        dbUserInfo.setName(userInfo.getName());
+        dbUserInfo.setSurname(userInfo.getSurname());
+        dbUserInfo.setAddress(userInfo.getAddress());
+        dbUserInfo.setUser(userRepository.findById(userId).get());
+        return  userInfoRepository.save(dbUserInfo);
+    }
+
 }
