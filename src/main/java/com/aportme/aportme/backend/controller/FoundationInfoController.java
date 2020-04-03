0bf928f85dc9537139ffefcc1b5750350e420188
poczekajcie <krakowski.jacek97@wp.pl>
2020-04-03 16:45:29 +0200

    add ApiOperations

--- src/main/java/com/aportme/aportme/backend/controller/FoundationInfoController.java
+++ src/main/java/com/aportme/aportme/backend/controller/FoundationInfoController.java
@@ -2,9 +2,10 @@ package com.aportme.aportme.backend.controller;
 
 import com.aportme.aportme.backend.dto.DTOEntity;
 import com.aportme.aportme.backend.dto.foundation.AddFoundationDTO;
+import com.aportme.aportme.backend.dto.foundation.FoundationInfoDTO;
 import com.aportme.aportme.backend.dto.foundation.UpdateFoundationDTO;
-import com.aportme.aportme.backend.entity.foundation.FoundationInfo;
 import com.aportme.aportme.backend.service.FoundationInfoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,31 @@ public class FoundationInfoController {
 
     private final FoundationInfoService foundationInfoService;
 
+    @ApiOperation(value = "Find all foundations", response = FoundationInfoDTO.class)
     @GetMapping
     public List<DTOEntity> getAll() {
         return foundationInfoService.getAll();
     }
 
+    @ApiOperation(value = "Find foundation by id", response = FoundationInfoDTO.class)
     @GetMapping("/{id}")
     public DTOEntity getById(@PathVariable Long id) {
         return foundationInfoService.getById(id);
     }
 
+    @ApiOperation(value = "Search  foundation by pet id", response = FoundationInfoDTO.class)
     @GetMapping("/search")
     public DTOEntity getByPetId(@RequestParam Long petId) {
         return foundationInfoService.getByPetId(petId);
     }
 
+    @ApiOperation(value = "Update foundation", response = FoundationInfoDTO.class)
     @PutMapping("/{id}")
     public DTOEntity update(@PathVariable Long id, @RequestBody UpdateFoundationDTO updateFoundationDTO) throws Exception {
         return foundationInfoService.update(id, updateFoundationDTO);
     }
 
+    @ApiOperation(value = "Create foundation", response = FoundationInfoDTO.class)
     @PostMapping
     public DTOEntity create(@RequestParam Long userId, @RequestBody AddFoundationDTO addFoundationDTO) throws Exception {
         return foundationInfoService.create(userId, addFoundationDTO);
