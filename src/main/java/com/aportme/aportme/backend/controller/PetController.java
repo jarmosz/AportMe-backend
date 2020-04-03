ad90a780395318aaed177b68a03be83046a26589
poczekajcie <krakowski.jacek97@wp.pl>
2020-04-03 16:45:29 +0200

    add ApiOperations

--- src/main/java/com/aportme/aportme/backend/controller/PetController.java
+++ src/main/java/com/aportme/aportme/backend/controller/PetController.java
@@ -2,9 +2,10 @@ package com.aportme.aportme.backend.controller;
 
 import com.aportme.aportme.backend.dto.DTOEntity;
 import com.aportme.aportme.backend.dto.pet.AddPetDTO;
+import com.aportme.aportme.backend.dto.pet.PetDTO;
 import com.aportme.aportme.backend.dto.pet.UpdatePetDTO;
-import com.aportme.aportme.backend.entity.pet.Pet;
 import com.aportme.aportme.backend.service.PetService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +19,25 @@ public class PetController {
 
     private final PetService petService;
 
+    @ApiOperation(value = "Find all pets", response = PetDTO.class)
     @GetMapping
     public List<DTOEntity> getAll() {
         return petService.getAll();
     }
 
+    @ApiOperation(value = "Find pet by id", response = PetDTO.class)
     @GetMapping("/{id}")
     public DTOEntity getById(@PathVariable Long id) {
         return petService.getById(id);
     }
 
+    @ApiOperation(value = "Update pet", response = PetDTO.class)
     @PutMapping("/{id}")
     public DTOEntity update(@PathVariable Long id, @RequestBody UpdatePetDTO petDTO) throws Exception {
         return petService.update(id, petDTO);
     }
 
+    @ApiOperation(value = "Create pet", response = PetDTO.class)
     @PostMapping
     public DTOEntity create(@RequestParam Long foundationId, @RequestBody AddPetDTO petDTO) throws Exception {
         return petService.create(foundationId, petDTO);
