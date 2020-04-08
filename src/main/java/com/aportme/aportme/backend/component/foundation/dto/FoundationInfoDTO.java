fc647ec7594d6d8f3e65cfd107772d1e3ce4816a
poczekajcie <krakowski.jacek97@wp.pl>
2020-04-08 15:27:51 +0200

    correct DTO

--- src/main/java/com/aportme/aportme/backend/component/foundation/dto/FoundationInfoDTO.java
+++ src/main/java/com/aportme/aportme/backend/component/foundation/dto/FoundationInfoDTO.java
@@ -1,9 +1,9 @@
 package com.aportme.aportme.backend.component.foundation.dto;
 
+import com.aportme.aportme.backend.component.address.dto.AddressDTO;
 import com.aportme.aportme.backend.utils.dto.DTOEntity;
 import com.aportme.aportme.backend.component.pet.dto.PetDTO;
 import com.aportme.aportme.backend.component.user.dto.UserDTO;
-import com.aportme.aportme.backend.component.address.entity.Address;
 import lombok.Data;
 
 import java.util.List;
@@ -21,7 +21,7 @@ public class FoundationInfoDTO implements DTOEntity {
 
     private UserDTO user;
 
-    private Address address;
+    private AddressDTO address;
 
     private List<PetDTO> pets;
 }
