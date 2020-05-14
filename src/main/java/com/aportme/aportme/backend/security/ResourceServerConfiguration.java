3df6c96cff6fc84c51866b54b004c8ef302d10ab
Wojciech Jarmosz <wojciech@jarmosz.com>
2020-05-14 16:53:46 +0200

    Basic spring Resource Server configuration for KeyCloak

---
+++ src/main/java/com/aportme/aportme/backend/security/ResourceServerConfiguration.java
@@ -0,0 +1,21 @@
+package com.aportme.aportme.backend.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .authorizeRequests()
+                .antMatchers("/api/foundations").hasAuthority("SCOPE_foundation")
+                .anyRequest().denyAll()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+
+}
