96c690bbf0b8c9437ac01e3b42155906d04e8a48
Wojciech Jarmosz <wojciech@jarmosz.com>
2020-05-23 15:21:10 +0200

    Adding template for exclude specific urls from authorization, upgrade for keycloack configuration

--- src/main/java/com/aportme/aportme/backend/security/ResourceServerConfiguration.java
+++ src/main/java/com/aportme/aportme/backend/security/ResourceServerConfiguration.java
@@ -1,11 +1,17 @@
 package com.aportme.aportme.backend.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/pets");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
