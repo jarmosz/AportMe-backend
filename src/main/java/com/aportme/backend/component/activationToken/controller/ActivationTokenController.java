ffa52a3ec958f74f33ade5766d0966d10a450bf6
Blvckhype <Muzyka90!>
2020-06-25 22:42:38 +0200

    adds resend activation mail functionality and small refactor of code

--- src/main/java/com/aportme/backend/component/activationToken/controller/ActivationTokenController.java
+++ src/main/java/com/aportme/backend/component/activationToken/controller/ActivationTokenController.java
@@ -5,18 +5,27 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/activateAccount")
 public class ActivationTokenController {
 
-    private ActivationTokenService activationTokenService;
+    private final ActivationTokenService activationTokenService;
 
-    @GetMapping("/activateAccount")
+    @GetMapping
     @ApiOperation("Activating user account by passing token in params.")
     public ResponseEntity<Object> activateAccount(@RequestParam("token") String token) {
         return activationTokenService.confirmActivationToken(token);
     }
+
+    @GetMapping("/resend")
+    @ApiOperation("Resend email activation token")
+    public ResponseEntity<Object> resendActivationMail(@RequestParam("email") String email) {
+        return activationTokenService.resendActivationToken(email);
+    }
+
 }
