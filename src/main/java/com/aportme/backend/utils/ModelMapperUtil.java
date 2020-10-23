8a36681329b14ada0cb96c7092cf892ca6834d49
Blvckhype <Muzyka90!>
2020-10-23 21:01:51 +0200

    adds ModelMapper strategy for searchable properties

--- src/main/java/com/aportme/backend/utils/ModelMapperUtil.java
+++ src/main/java/com/aportme/backend/utils/ModelMapperUtil.java
@@ -1,7 +1,9 @@
 package com.aportme.backend.utils;
 
 import com.aportme.backend.entity.Foundation;
+import com.aportme.backend.entity.Pet;
 import com.aportme.backend.entity.dto.foundation.FoundationDTO;
+import com.aportme.backend.entity.dto.pet.PetDTO;
 import org.modelmapper.ModelMapper;
 
 public class ModelMapperUtil {
@@ -9,5 +11,13 @@ public class ModelMapperUtil {
     public static void mapUserToFoundationDTO(ModelMapper modelMapper) {
         modelMapper.typeMap(Foundation.class, FoundationDTO.class)
                 .addMappings(mapper -> mapper.map(src -> src.getUser().getEmail(), FoundationDTO::setEmail));
+    }
+
+    public static void mapPetDTOtoPet(ModelMapper modelMapper) {
+        modelMapper.typeMap(PetDTO.class, Pet.class)
+                .addMappings(mapper -> {
+                    mapper.map(src -> src.getName().toLowerCase(), Pet::setSearchableName);
+                    mapper.map(src -> src.getBreed().toLowerCase(), Pet::setSearchableBreed);
+                });
     }
 }
