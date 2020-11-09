8dbd0e1ee7a0c82e75da40aac2820f8c58882c92
Jacek Krakowski <jacek.krakowski@apilia.pl>
2020-11-09 16:47:12 +0100

    Cleanup after rebase

--- src/main/java/com/aportme/backend/service/UserService.java
+++ src/main/java/com/aportme/backend/service/UserService.java
@@ -3,7 +3,6 @@ package com.aportme.backend.service;
 import com.aportme.backend.entity.User;
 import com.aportme.backend.entity.dto.user.AuthUserDTO;
 import com.aportme.backend.entity.dto.user.ChangeUserPasswordDTO;
-import com.aportme.backend.entity.dto.user.UserDTO;
 import com.aportme.backend.event.OnRegistrationCompleteEvent;
 import com.aportme.backend.exception.UserAlreadyExistsException;
 import com.aportme.backend.exception.UserNotFoundException;
@@ -26,7 +25,6 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final ApplicationEventPublisher eventPublisher;
     private final ModelMapper modelMapper;
-    private final AuthenticationService authenticationService;
 
     private static final String EMAIL_REGEX_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
     private static final String PASSWORD_REGEX_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,15}$";
@@ -66,14 +64,8 @@ public class UserService {
         }
     }
 
-    private User findByEmail(){
-        String userEmail = authenticationService.getLoggedUserName();
-        User user = userRepository.findByEmail(userEmail).orElseThrow(UserNotFoundException::new);
-        return user;
-    }
-
     public void changeUserPassword(ChangeUserPasswordDTO passwords) {
-        User loggedUser = findByEmail();
+        User loggedUser = getLoggedUser();
         String encodedNewPassword = passwordEncoder.encode(passwords.getNewPassword());
         if (isNewPasswordDataValid(passwords, loggedUser)) {
             loggedUser.setPassword(encodedNewPassword);
