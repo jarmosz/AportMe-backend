95387b736fee45e8af6b3bac095edf6f99c56adf
Mateusz Lesiecki <mateusz.lesiecki@apilia.pl>
2020-11-18 17:04:49 +0100

    store user role, id and email in jwt token

--- src/main/java/com/aportme/backend/security/JWTAuthentication.java
+++ src/main/java/com/aportme/backend/security/JWTAuthentication.java
@@ -1,10 +1,11 @@
 package com.aportme.backend.security;
 
-import com.aportme.backend.entity.User;
+import com.aportme.backend.entity.enums.Role;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class JWTAuthentication implements Authentication {
 
@@ -12,10 +13,10 @@ public class JWTAuthentication implements Authentication {
     private final String email;
     private final Collection<GrantedAuthority> grantedAuthorities;
 
-    public JWTAuthentication(User user) {
-        this.userId = user.getId();
-        this.email = user.getEmail();
-        this.grantedAuthorities = user.getAuthorities();
+    public JWTAuthentication(Long id, String email, Role role) {
+        this.userId = id;
+        this.email = email;
+        this.grantedAuthorities = Collections.singleton(role);
     }
 
     @Override
@@ -31,10 +32,6 @@ public class JWTAuthentication implements Authentication {
     @Override
     public Object getDetails() {
         return null;
-    }
-
-    public long getUserId() {
-        return userId;
     }
 
     @Override
