c7031d8cce7104ed9029d33c201ee1a5c21590f6
Jacek Krakowski <jacek.krakowski@apilia.pl>
2020-11-23 17:44:35 +0100

    Move checking if user like pet to service

--- src/main/java/com/aportme/backend/service/LikeService.java
+++ src/main/java/com/aportme/backend/service/LikeService.java
@@ -7,12 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class LikeService {
 
     private final UserService userService;
     private final PetService petService;
+    private final AuthenticationService authenticationService;
 
     public ResponseEntity<Object> like(Long petId) {
         Pet pet = petService.findById(petId);
@@ -23,11 +27,15 @@ public class LikeService {
     }
 
     public ResponseEntity<Object> unlike(Long petId) {
-        Pet pet = petService.findById(petId);
-        User user = userService.getLoggedUser();
-        user.getLikedPets().remove(pet);
-        userService.saveUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        if (arePetLikedByUser(petId)) {
+            Pet pet = petService.findById(petId);
+            User user = userService.getLoggedUser();
+            user.getLikedPets().remove(pet);
+            userService.saveUser(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     public ResponseEntity<Object> clearLikes() {
@@ -35,5 +43,14 @@ public class LikeService {
         user.getLikedPets().clear();
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Boolean arePetLikedByUser(Long petId) {
+        Pet pet = petService.findById(petId);
+        List<String> userEmails = pet.getUsers()
+                .stream()
+                .map(User::getEmail)
+                .collect(Collectors.toList());
+        return userEmails.contains(authenticationService.getAuthentication().getName());
     }
 }
