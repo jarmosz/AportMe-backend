2d00e4d8def20fe3a14eb10b11a96fe6e677e82f
Mateusz Lesiecki <mateusz.lesiecki@apilia.pl>
2021-01-05 19:56:40 +0100

    fixes error with adding new user surveys if foundation block it meantime and return error if user request for survey questions of inactive foundation survey

--- src/main/java/com/aportme/backend/advice/SurveysControllerAdvice.java
+++ src/main/java/com/aportme/backend/advice/SurveysControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.aportme.backend.advice;
 
+import com.aportme.backend.exception.FoundationSurveyInactiveException;
 import com.aportme.backend.exception.UserSurveyWithoutDecisionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,4 +15,10 @@ public class SurveysControllerAdvice {
         return new ResponseEntity<>("You have unresolved surveys", HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(FoundationSurveyInactiveException.class)
+    public ResponseEntity<Object> foundationSurveyInactive() {
+//        Flutter rzuca ExceptionFormat jeżeli idzie tutaj wiadomość nie mam pojęcia dlaczego
+//        return new ResponseEntity<>("Foundation survey is inactive unable to get questions", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
