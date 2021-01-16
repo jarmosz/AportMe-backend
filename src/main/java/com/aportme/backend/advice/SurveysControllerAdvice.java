e09191ecc5680150997f2d845e1b0bac4b86c026
Mateusz Lesiecki <mateusz.lesiecki@apilia.pl>
2021-01-16 17:23:14 +0100

    deletes blockade to withdraw foundation survey if there are unresolved user surveys

--- src/main/java/com/aportme/backend/advice/SurveysControllerAdvice.java
+++ src/main/java/com/aportme/backend/advice/SurveysControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.aportme.backend.advice;
 
 import com.aportme.backend.exception.FoundationSurveyInactiveException;
-import com.aportme.backend.exception.UserSurveyWithoutDecisionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class SurveysControllerAdvice {
-
-    @ExceptionHandler(UserSurveyWithoutDecisionException.class)
-    public ResponseEntity<Object> unresolvedSurvey() {
-        return new ResponseEntity<>("You have unresolved surveys", HttpStatus.CONFLICT);
-    }
 
     @ExceptionHandler(FoundationSurveyInactiveException.class)
     public ResponseEntity<Object> foundationSurveyInactive() {
